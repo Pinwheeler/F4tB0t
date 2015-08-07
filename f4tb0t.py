510ca577a342db37f342eb9dad1a0e06f6d042a5
Anthony Dreessen <anthonydreessen@gmail.com>
2015-08-06 20:30:50 -0700

    Work with Raleigh and Nate

--- f4tb0t.py
+++ f4tb0t.py
@@ -8,6 +8,9 @@ from bot_config import *
 from IPython import embed
 from nltk import *
 
+KILOGRAMS = "kilograms"
+POUNDS = "pounds"
+
 if not os.path.isfile('bot_config.py'):
   print "You must create config file with your reddit username and password."
   print "Please see config_skel.py"
@@ -31,6 +34,24 @@ else:
 
 post = r.get_submission(POST_ENDPOINT)
 
+class WeightResponse(object):
+  """WeightResponse takes a list of tuples and packages it nicely into an object"""
+  def __init__(self, responseTuple):
+    super(WeightResponse, self).__init__()
+    self.responseTuple = responseTuple
+    self.weight = getWeightValue(self.responseTuple)
+    self.units  = getUnits(self.responseTuple)
+  def weight(self):
+    return self.weight
+  def units(self):
+    return self.units
+  def weight_in_pounds(self):
+    if self.units == KILOGRAMS:
+      return self.weight * 2.2
+    else:
+      return self.weight
+
+
 def extract(list):
   items = []
   while list[list.count] is not '':
@@ -39,12 +60,27 @@ def extract(list):
   return items
 
 def weightFromText(text):
-  regex = re.compile('([0-9]+)')
+  regex = re.compile('(([0-9]+) ?([lpkLPK][\w]+))')
   return regex.findall(text)
 
-for comment in post.comments[1:]:
-  print comment.author
-  name = comment.author
-  weight = weightFromText(comment.body)
-  print weight
+def main():
+  for comment in post.comments[1:]:
+    print comment.author
+    name = comment.author
+    weightResponse = WeightResponse(weightFromText(comment.body)[0])
+    print weightResponse.weight_in_pounds()
 
+def getUnits(l):
+  if l[2][0] == 'k':
+    return KILOGRAMS
+  elif l[2][0] == 'l' or 'p':
+    return POUNDS
+
+def getWeightValue(l):
+  return float(l[1])
+
+
+
+
+
+main()
