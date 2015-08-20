5240ffbffa39183d01dbc30532892db6bb2542d4
Nate Faubion <nfaubion28@hotmail.com>
2015-08-19 19:13:53 -0700

    Creates Dictionary of subreddits' avg. weight

--- bot_config.py
+++ bot_config.py
@@ -1,4 +1,4 @@
 REDDIT_USERNAME = 'F4tB0t'
 REDDIT_PASS = 'hedonism'
-POST_ENDPOINT = 'https://www.reddit.com/r/AskReddit/comments/dlvjk/reddit_how_tall_are_you_and_how_much_so_you/'
-OLD_POST_ENDPOINT = 'https://www.reddit.com/r/AskReddit/comments/35s82x/serious_what_is_your_weight/'
+xPOST_ENDPOINT = 'http://www.reddit.com/r/AskReddit/comments/dlvjk/reddit_how_tall_are_you_and_how_much_so_you/'
+POST_ENDPOINT = 'https://www.reddit.com/r/bodyweightfitness/comments/1tqxmg/rbwfitness_how_much_do_you_weigh/'
--- f4tb0t.py
+++ f4tb0t.py
@@ -20,7 +20,7 @@ user_agent = ("F4tB0t v0.1")
 r = praw.Reddit(user_agent=user_agent)
 r.login(REDDIT_USERNAME, REDDIT_PASS)
 
-if not os.path.isfile("users.csv"):
+"""if not os.path.isfile("users.csv"):
   users = []
 else:
   with open("users.csv", "rb") as f:
@@ -30,11 +30,9 @@ else:
       user = {}
       user.username = row['username']
       user.weight   = row['weight']
-      users.append(user)
+      users.append(user)"""
 
 post = r.get_submission(POST_ENDPOINT)
-
-print post
 
 def weightFromText(text):
   regex = re.compile('(([0-9]+) ?([lpkLPK][\w]+))')
@@ -43,34 +41,60 @@ def weightFromText(text):
 def main():
   good_entries = 0
   bad_entries = 0
-  rawlist = [] 
+  weightlist = [] 
   namelist = []
+  subs = []
   for comment in post.comments[1:]:
     name = comment.author
     try:
       weightResponse = WeightResponse(weightFromText(comment.body)[0])
       weightResponse.author = name 
-      weightResponse.pretty_print()
+      #weightResponse.pretty_print()
       good_entries = good_entries + 1 
       if weightResponse.is_this_reasonable():
-        rawlist.append(weightResponse.weight_in_pounds()) 
+        weightlist.append(weightResponse.weight_in_pounds()) 
         namelist.append(weightResponse.author)
       else:
         pass
-           
     except Exception, e:
       bad_entries = bad_entries + 1
     
-  print namelist
-  print "The average weight of commenters is %d pounds" % int(mean(rawlist))
-  totEnt = good_entries + bad_entries
-  print "There were %d entries of %d total without data" % (bad_entries, totEnt)
+  print "The average weight of commenters is %d pounds" % int(mean(weightlist))
+  tot_entries = good_entries + bad_entries
+  print "There were %d entries of %d total without data" % (bad_entries, tot_entries)
   for name in namelist:
     try:
       user = r.get_redditor(name)
-      print subredditsForUser(user)
+      subs.append(subredditsForUser(user))
     except Exception, e:
       pass
+  print compiler(subs, weightlist)
+
+def compiler(subs, weight):
+  dictionary = {}
+  for x in range(len(subs)):
+    for y in range(len(weight)):
+      if x == y:
+        for z in subs[x]:
+          if z in dictionary:
+            dictionary[z].append(weight[y])
+          else:
+            dictionary[z] = [weight[y]]
+      else:
+        pass
+  for d in dictionary:
+    dictionary[d] = mean(dictionary[d])
+  return dictionary
+
+def condense(list):  
+  for x in range(len(list)):
+    for y in range(len(list)):
+      if list[x] != "Null" and list[x] == list[y] and y > x:
+        list[y] = "Null"
+      else:
+        pass
+  list[:] = [value for value in list if value != "Null"]
+  return list
 
 def commentsForUser(user):
   return user.get_comments()
@@ -82,7 +106,7 @@ def subredditsForUser(user):
   subreddits = []
   for comment in commentsForUser(user):
     subreddits.append(subredditForComment(comment))
-  return subreddits
+  return condense(subreddits)
 
 def mean(list):
   summ = 0
